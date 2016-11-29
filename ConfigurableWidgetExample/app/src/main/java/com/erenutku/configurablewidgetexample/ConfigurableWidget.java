f4c468650b644dd2a7e353ff25c75a22e6bc16bb
yerenutku <yerenutku@gmail.com>
2016-11-29 17:23:52 +0300

    ConfigurableWidget.java cleaning

--- ConfigurableWidgetExample/app/src/main/java/com/erenutku/configurablewidgetexample/ConfigurableWidget.java
+++ ConfigurableWidgetExample/app/src/main/java/com/erenutku/configurablewidgetexample/ConfigurableWidget.java
@@ -10,32 +10,12 @@ import android.content.Context;
  */
 public class ConfigurableWidget extends AppWidgetProvider {
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
-
-        // Construct the RemoteViews object
-//        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.configurable_widget);
-//        views.setOnClickPendingIntent();
-
-        // Instruct the widget manager to update the widget
-//        appWidgetManager.updateAppWidget(appWidgetId, views);
-    }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        // There may be multiple widgets active, so update all of them
-        for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
-        }
+        //do nothing
     }
 
-    @Override
-    public void onDeleted(Context context, int[] appWidgetIds) {
-        // When the user deletes the widget, delete the preference associated with it.
-        for (int appWidgetId : appWidgetIds) {
-//            ConfigurableWidgetConfigureActivity.deleteTitlePref(context, appWidgetId);
-        }
-    }
 
 }
 
