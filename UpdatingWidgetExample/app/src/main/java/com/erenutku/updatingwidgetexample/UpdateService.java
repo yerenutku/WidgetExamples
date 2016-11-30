49b7a4d39067441b5fde1c7cfb95dec1d946b655
yerenutku <yerenutku@gmail.com>
2016-11-30 13:49:10 +0300

    finishing updating widget example.

---
+++ UpdatingWidgetExample/app/src/main/java/com/erenutku/updatingwidgetexample/UpdateService.java
@@ -0,0 +1,39 @@
+package com.erenutku.updatingwidgetexample;
+
+import android.app.Service;
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.widget.RemoteViews;
+
+import java.util.Random;
+
+/**
+ * Created by yutku on 29/11/16.
+ */
+
+public class UpdateService extends Service {
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        Random random = new Random();
+        int randomInt = random.nextInt(100);
+        String lastUpdate = "R: "+randomInt;
+
+        RemoteViews view = new RemoteViews(getPackageName(), R.layout.updating_widget);
+        view.setTextViewText(R.id.tvWidget, lastUpdate);
+        ComponentName theWidget = new ComponentName(this, UpdatingWidget.class);
+        AppWidgetManager manager = AppWidgetManager.getInstance(this);
+        manager.updateAppWidget(theWidget, view);
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+}
---
+++ UpdatingWidgetExample/app/src/main/java/com/erenutku/updatingwidgetexample/UpdatingWidget.java
@@ -0,0 +1,79 @@
+package com.erenutku.updatingwidgetexample;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.util.Log;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class UpdatingWidget extends AppWidgetProvider {
+    private PendingIntent service;
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        final AlarmManager manager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        final Intent i = new Intent(context, UpdateService.class);
+
+        if (service == null) {
+            service = PendingIntent.getService(context, 0, i, PendingIntent.FLAG_CANCEL_CURRENT);
+        }
+        manager.setRepeating(AlarmManager.ELAPSED_REALTIME, SystemClock.elapsedRealtime(), 60000, service);
+        //if you need to call your service less then 60 sec
+        //answer is here:
+        //http://stackoverflow.com/questions/29998313/how-to-run-background-service-after-every-5-sec-not-working-in-android-5-1
+        Log.d("UpdatingWidget: ","onUpdate");
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+        Log.d("UpdatingWidget: ","onReceive");
+
+    }
+
+    @Override
+    public void onAppWidgetOptionsChanged(Context context, AppWidgetManager appWidgetManager, int appWidgetId, Bundle newOptions) {
+        super.onAppWidgetOptionsChanged(context, appWidgetManager, appWidgetId, newOptions);
+        Log.d("UpdatingWidget: ","onAppWidgetOptionsChanged");
+
+    }
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        super.onDeleted(context, appWidgetIds);
+        Log.d("UpdatingWidget: ","onDeleted");
+
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        super.onEnabled(context);
+        Log.d("UpdatingWidget: ","onEnabled");
+
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        super.onDisabled(context);
+        Log.d("UpdatingWidget: ","onDisabled");
+
+    }
+
+    @Override
+    public void onRestored(Context context, int[] oldWidgetIds, int[] newWidgetIds) {
+        super.onRestored(context, oldWidgetIds, newWidgetIds);
+        Log.d("UpdatingWidget: ","onRestored");
+
+    }
+}
+
+
+
+
